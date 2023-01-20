89fa603a95314971d2208f53fa7ba14fa9a2eac7
mireyachaparro <mireyacn99@gmail.com>
2023-01-20 12:52:07 +0100

    doing routes files

--- src/infrastructure/components/app/App.tsx
+++ src/infrastructure/components/app/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import { Header } from "../header/header";
-import PokeList from "../pokemonsList/pokemonsList";
 import { AppRoutes } from "../routes/app.routes";
 
 function App() {
