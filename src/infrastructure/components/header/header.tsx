21f3f38b1b684aa1de6c59239f999466f07b5f30
mireyachaparro <mireyacn99@gmail.com>
2023-01-25 16:15:35 +0100

    Add header style

--- src/infrastructure/components/header/header.tsx
+++ src/infrastructure/components/header/header.tsx
@@ -1,10 +1,15 @@
-import style from "./header.module.css";
+// import style from "./header.module.css";
 
 export const Header = () => {
     return (
         <>
-            <header className={style.header}>
-                <img src="./assets/logo.png" alt="pokemon"></img>
+            {/* <header className={style.header}> */}
+            <header className="container-fluid row bg-danger sticky-top mx-0 px-0">
+                <img
+                    src="./assets/logo.png"
+                    alt="pokemon"
+                    className="w-25 rounded mx-auto d-block py-3"
+                ></img>
             </header>
         </>
     );
