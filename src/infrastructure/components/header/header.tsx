b34877327bc54cf67b8ee3ec5cd7486a9a6f872b
mireyachaparro <mireyacn99@gmail.com>
2023-02-01 13:55:53 +0100

    Add link to home in header

--- src/infrastructure/components/header/header.tsx
+++ src/infrastructure/components/header/header.tsx
@@ -1,15 +1,16 @@
-// import style from "./header.module.css";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
     return (
         <>
-            {/* <header className={style.header}> */}
             <header className="container-fluid row bg-danger sticky-top mx-0 px-0">
-                <img
-                    src="./assets/logo.png"
-                    alt="pokemon"
-                    className="w-25 rounded mx-auto d-block py-3"
-                ></img>
+                <Link to="/">
+                    <img
+                        src="./assets/logo.png"
+                        alt="pokemon"
+                        className="w-25 rounded mx-auto d-block py-3"
+                    ></img>
+                </Link>
             </header>
         </>
     );
