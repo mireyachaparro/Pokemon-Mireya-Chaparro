b3420377e3630d5bbf6b5ef42fd6a05850b0a567
mireyachaparro <mireyacn99@gmail.com>
2023-02-01 13:01:04 +0100

    Add pokemon list style

--- src/infrastructure/components/pokemonsList/pokeItem.tsx
+++ src/infrastructure/components/pokemonsList/pokeItem.tsx
@@ -4,15 +4,16 @@ import { PokeInitial } from "../../model/poke.model";
 
 function pokeItem({ pokemon }: { pokemon: PokeInitial }) {
     return (
-        <li>
-            <div>
-                {/* <Link to={"/pokemons/" + pokemon.id} key={pokemon.id}> */}
+        <li className=" border border-3 border-info-subtle mt-2 rounded-2">
+            <div className="d-flex align-self-center bg-info-subtle">
                 <Link
                     to={"/pokemons/" + pokemon.name.toLowerCase()}
                     key={pokemon.name}
                 >
-                    <p>{pokemon.name}</p>
-                    <img src={pokemon.imgSrc} alt={pokemon.name} />
+                    <p className="text-dark d-flex justify-content-center bg-warning-subtle rounded-top-1 py-2 border-bottom border-info-subtle">
+                        {pokemon.name}
+                    </p>
+                    <img src={pokemon.imgSrc} alt={pokemon.name} width="120" />
                 </Link>
             </div>
         </li>
